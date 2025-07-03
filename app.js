11cc01f310292e3e11e141a2eaf87b59df58ca5e
bobby2510 <kvp.babby@gmail.com>
2025-07-03 21:13:05 +0530

    todo apis app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const bookRoutes = require('./routes/bookRoutes');
 const authorRoutes = require('./routes/authorRoutes');
+const todoRoutes = require('./routes/todoRoutes')
 require('dotenv').config();
 const cors = require('cors')
 
@@ -13,6 +14,7 @@ app.use(cors())
 app.use(bodyParser.json());
 app.use('/api/books', bookRoutes);
 app.use('/api/authors', authorRoutes);
+app.use('/api/todos', todoRoutes);
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
