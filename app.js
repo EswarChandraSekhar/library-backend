6d158d52239ffac29627f36b45f85a7c208d3fa6
bobby2510 <kvp.babby@gmail.com>
2025-07-09 21:23:24 +0530

    Revert "login auth added"
    This reverts commit 5cba032f03d282a3f367ae52c426c7b65e2b5d94.

--- app.js
+++ app.js
@@ -4,22 +4,17 @@ const bodyParser = require('body-parser');
 const bookRoutes = require('./routes/bookRoutes');
 const authorRoutes = require('./routes/authorRoutes');
 const todoRoutes = require('./routes/todoRoutes')
-const authRoutes = require('./routes/authRoutes')
 require('dotenv').config();
 const cors = require('cors')
-const helmet = require('helmet')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors())
-app.use(helmet())
 // Middleware
 app.use(bodyParser.json());
 app.use('/api/books', bookRoutes);
 app.use('/api/authors', authorRoutes);
 app.use('/api/todos', todoRoutes);
-app.use('/api/auth',authRoutes)
-
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
